f2d9fd78a6f2a0894492fc615941590c0a996cd2
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-05-09 16:36:55 +0000

    Better code

--- countLetters.js
+++ countLetters.js
@@ -3,12 +3,12 @@ var letterCount = {};
 function countLetters(count) {
   count = count.split(" ").join("");
   for(i = 0; i < count.length;i++) {
-    letterCount[count[i]] = 0
+    if(letterCount[count[i]]) {
+      letterCount[count[i]] += 1
+    } else {
+      letterCount[count[i]] = 1
+    }
   }
-  for(i = 0; i < count.length;i++) {
-    letterCount[count[i]] += 1
-  }
-
 }
 countLetters("lighthouse in the house")
 console.log(letterCount);
