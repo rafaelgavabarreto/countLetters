73954c072a8cdd6267c1ac7d0c8fb27f982a93e4
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-05-09 16:09:58 +0000

    Final

---
+++ countLetters.js
@@ -0,0 +1,14 @@
+var letterCount = {};
+
+function countLetters(count) {
+  count = count.split(" ").join("");
+  for(i = 0; i < count.length;i++) {
+    letterCount[count[i]] = 0
+  }
+  for(i = 0; i < count.length;i++) {
+    letterCount[count[i]] += 1
+  }
+
+}
+countLetters("lighthouse in the house")
+console.log(letterCount);
